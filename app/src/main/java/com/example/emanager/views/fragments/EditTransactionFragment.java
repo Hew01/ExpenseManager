9faa356228fab4c3c33abf75edb675b41a9b953e
Hew01 <maiphuctam62@gmail.com>
2024-05-17 17:53:53 +0700

    Update EditTransactionFragment.java

--- app/src/main/java/com/example/emanager/views/fragments/EditTransactionFragment.java
+++ app/src/main/java/com/example/emanager/views/fragments/EditTransactionFragment.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -28,38 +29,89 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+
+import io.realm.Realm;
+
 public class EditTransactionFragment extends BottomSheetDialogFragment {
     Transaction transaction;
     FragmentAddTransactionBinding binding;
+    Realm realm;
     public EditTransactionFragment(Transaction transaction) {
         this.transaction = transaction;
     }
-
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Realm.init(getContext());  // Initialize Realm
+        realm = Realm.getDefaultInstance();  // Get a Realm instance
+    }
     @Override
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentAddTransactionBinding.inflate(inflater);
-        transaction = new Transaction();
 
-        binding.incomeBtn.setOnClickListener(view -> {
+        binding.amount.setText(String.valueOf(transaction.getAmount()));
+        binding.note.setText(transaction.getNote());
+        binding.date.setText(Helper.formatDate(transaction.getDate()));
+        binding.category.setText(transaction.getCategory());
+        binding.account.setText(transaction.getAccount());
+
+        if (transaction.getType().equals(Constants.INCOME)) {
             binding.incomeBtn.setBackground(getContext().getDrawable(R.drawable.income_selector));
             binding.expenseBtn.setBackground(getContext().getDrawable(R.drawable.default_selector));
             binding.expenseBtn.setTextColor(getContext().getColor(R.color.textColor));
             binding.incomeBtn.setTextColor(getContext().getColor(R.color.greenColor));
-
-            transaction.setType(Constants.INCOME);
-        });
-
-        binding.expenseBtn.setOnClickListener(view -> {
+        } else if (transaction.getType().equals(Constants.EXPENSE)) {
             binding.incomeBtn.setBackground(getContext().getDrawable(R.drawable.default_selector));
             binding.expenseBtn.setBackground(getContext().getDrawable(R.drawable.expense_selector));
             binding.incomeBtn.setTextColor(getContext().getColor(R.color.textColor));
             binding.expenseBtn.setTextColor(getContext().getColor(R.color.redColor));
+        }
 
-            transaction.setType(Constants.EXPENSE);
+        binding.incomeBtn.setOnClickListener(view -> {
+            final long transactionId = transaction.getId();  // Get the id on the UI thread
+
+            realm.executeTransactionAsync(new Realm.Transaction() {
+                @Override
+                public void execute(Realm realm) {
+                    // Fetch the Transaction object within the transaction
+                    Transaction transactionToUpdate = realm.where(Transaction.class)
+                            .equalTo("id", transactionId)  // Use the id we got on the UI thread
+                            .findFirst();
+
+                    if (transactionToUpdate != null) {
+                        transactionToUpdate.setType(Constants.INCOME);
+                    }
+                }
+            });
+            binding.incomeBtn.setBackground(getContext().getDrawable(R.drawable.income_selector));
+            binding.expenseBtn.setBackground(getContext().getDrawable(R.drawable.default_selector));
+            binding.expenseBtn.setTextColor(getContext().getColor(R.color.textColor));
+            binding.incomeBtn.setTextColor(getContext().getColor(R.color.greenColor));
         });
 
+        binding.expenseBtn.setOnClickListener(view -> {
+            final long transactionId = transaction.getId();  // Get the id on the UI thread
+
+            realm.executeTransactionAsync(new Realm.Transaction() {
+                @Override
+                public void execute(Realm realm) {
+                    // Fetch the Transaction object within the transaction
+                    Transaction transactionToUpdate = realm.where(Transaction.class)
+                            .equalTo("id", transactionId)  // Use the id we got on the UI thread
+                            .findFirst();
+
+                    if (transactionToUpdate != null) {
+                        transactionToUpdate.setType(Constants.EXPENSE);
+                    }
+                }
+            });
+            binding.incomeBtn.setBackground(getContext().getDrawable(R.drawable.default_selector));
+            binding.expenseBtn.setBackground(getContext().getDrawable(R.drawable.expense_selector));
+            binding.incomeBtn.setTextColor(getContext().getColor(R.color.textColor));
+            binding.expenseBtn.setTextColor(getContext().getColor(R.color.redColor));
+        });
         binding.date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -86,14 +138,26 @@ public class EditTransactionFragment extends BottomSheetDialogFragment {
             ListDialogBinding dialogBinding = ListDialogBinding.inflate(inflater);
             AlertDialog categoryDialog = new AlertDialog.Builder(getContext()).create();
             categoryDialog.setView(dialogBinding.getRoot());
-
-
-
             CategoryAdapter categoryAdapter = new CategoryAdapter(getContext(), Constants.categories, new CategoryAdapter.CategoryClickListener() {
                 @Override
                 public void onCategoryClicked(Category category) {
                     binding.category.setText(category.getCategoryName());
-                    transaction.setCategory(category.getCategoryName());
+                    final String categoryName = category.getCategoryName();  // Get the category name on the UI thread
+                    final long transactionId = transaction.getId();  // Get the id on the UI thread
+
+                    realm.executeTransactionAsync(new Realm.Transaction() {
+                        @Override
+                        public void execute(Realm realm) {
+                            // Fetch the Transaction object within the transaction
+                            Transaction transactionToUpdate = realm.where(Transaction.class)
+                                    .equalTo("id", transactionId)  // Use the id we got on the UI thread
+                                    .findFirst();
+
+                            if (transactionToUpdate != null) {
+                                transactionToUpdate.setCategory(categoryName);  // Use the category name we got on the UI thread
+                            }
+                        }
+                    });
                     categoryDialog.dismiss();
                 }
             });
@@ -118,7 +182,22 @@ public class EditTransactionFragment extends BottomSheetDialogFragment {
                 @Override
                 public void onAccountSelected(Account account) {
                     binding.account.setText(account.getAccountName());
-                    transaction.setAccount(account.getAccountName());
+                    final String accountName = account.getAccountName();  // Get the account name on the UI thread
+                    final long transactionId = transaction.getId();  // Get the id on the UI thread
+
+                    realm.executeTransactionAsync(new Realm.Transaction() {
+                        @Override
+                        public void execute(Realm realm) {
+                            // Fetch the Transaction object within the transaction
+                            Transaction transactionToUpdate = realm.where(Transaction.class)
+                                    .equalTo("id", transactionId)  // Use the id we got on the UI thread
+                                    .findFirst();
+
+                            if (transactionToUpdate != null) {
+                                transactionToUpdate.setAccount(accountName);  // Use the account name we got on the UI thread
+                            }
+                        }
+                    });
                     accountsDialog.dismiss();
                 }
             });
@@ -131,21 +210,48 @@ public class EditTransactionFragment extends BottomSheetDialogFragment {
         });
 
         binding.saveTransactionBtn.setOnClickListener(c-> {
-            double amount = Double.parseDouble(binding.amount.getText().toString());
-            String note = binding.note.getText().toString();
+            final long transactionId = transaction.getId();  // Get the id on the UI thread
+            final String transactionType = transaction.getType();  // Get the transaction type on the UI thread
+            realm.executeTransactionAsync(new Realm.Transaction() {
+                @Override
+                public void execute(Realm realm) {
+                    // Fetch the Transaction object within the transaction
+                    Transaction transactionToUpdate = realm.where(Transaction.class)
+                            .equalTo("id", transactionId)  // Use the id we got on the UI thread
+                            .findFirst();
 
-            if(transaction.getType().equals(Constants.EXPENSE)) {
-                transaction.setAmount(amount*-1);
-            } else {
-                transaction.setAmount(amount);
-            }
+                    if (transactionToUpdate != null) {
+                        double amount = Double.parseDouble(binding.amount.getText().toString());
+                        if (transactionType.equals(Constants.EXPENSE)) {
+                            transactionToUpdate.setAmount(amount * -1);
+                        } else {
+                            transactionToUpdate.setAmount(amount);
+                        }
 
-            transaction.setNote(note);
+                        String note = binding.note.getText().toString();
+                        transactionToUpdate.setNote(note);
+                    }
+                }
+            }, new Realm.Transaction.OnSuccess() {
+                @Override
+                public void onSuccess() {
+                    // Update the UI here...
+                    MainActivity mainActivity = (MainActivity) getActivity();
+                    if (mainActivity != null) {
+                        mainActivity.getTransactions();
+                    }
 
-            ((MainActivity)getActivity()).viewModel.addTransaction(transaction);
-            ((MainActivity)getActivity()).getTransactions();
-            dismiss();
+                    dismiss();
+                }
+            });
+
         });
+
         return binding.getRoot();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        realm.close();  // Close the Realm instance when you're done with it
     }
 }
